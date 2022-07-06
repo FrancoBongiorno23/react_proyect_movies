8ad7dc08238b035efdcea4e884607a98cb7cd7fa
Franco Bongiorno <francobongiorno306@gmail.com>
2022-07-06 12:56:37 -0300

    se pasa a un archivo json nuevo las peliculas y se linkea con la hoja de apps.js

--- src/PageWrapper.js
+++ src/PageWrapper.js
@@ -221,50 +221,7 @@ export default function PageWrapper(props) {
                         {props.children}
         
                 
-                        <div className="movie-item-style-2">
-                            <img src="images/uploads/mv2.jpg" alt=""/>
-                            <div className="mv-item-infor">
-                                <h6><a href="moviesingle.html">into the wild <span>(2014)</span></a></h6>
-                                <p className="rate"><i className="ion-android-star"></i><span>7.8</span> /10</p>
-                                <p className="describe">As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat...</p>
-                                <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-                                <p>Director: <a href="#">Anthony Russo,</a><a href="#">Joe Russo</a></p>
-                                <p>Stars: <a href="#">Chris Evans,</a> <a href="#">Samuel L. Jackson,</a> <a href="#">  Scarlett Johansson</a></p>
-                            </div>
-                        </div>
-                        <div className="movie-item-style-2">
-                            <img src="images/uploads/mv3.jpg" alt=""/>
-                            <div className="mv-item-infor">
-                                <h6><a href="moviesingle.html">blade runner  <span>(2015)</span></a></h6>
-                                <p className="rate"><i className="ion-android-star"></i><span>7.3</span> /10</p>
-                                <p className="describe">Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help...</p>
-                                <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-                                <p>Director: <a href="#">Peyton Reed</a></p>
-                                <p>Stars: <a href="#">Paul Rudd,</a> <a href="#"> Michael Douglas</a></p>
-                            </div>
-                        </div>
-                        <div className="movie-item-style-2">
-                            <img src="images/uploads/mv4.jpg" alt=""/>
-                            <div className="mv-item-infor">
-                                <h6><a href="moviesingle.html">Mulholland pride<span> (2013)  </span></a></h6>
-                                <p className="rate"><i className="ion-android-star"></i><span>7.2</span> /10</p>
-                                <p className="describe">When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.</p>
-                                <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-                                <p>Director: <a href="#">Shane Black</a></p>
-                                <p>Stars: <a href="#">Robert Downey Jr., </a> <a href="#">  Guy Pearce,</a><a href="#">Don Cheadle</a></p>
-                            </div>
-                        </div>
-                        <div className="movie-item-style-2">
-                            <img src="images/uploads/mv5.jpg" alt=""/>
-                            <div className="mv-item-infor">
-                                <h6><a href="moviesingle.html">skyfall: evil of boss<span> (2013)  </span></a></h6>
-                                <p className="rate"><i className="ion-android-star"></i><span>7.0</span> /10</p>
-                                <p className="describe">When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.</p>
-                                <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-                                <p>Director: <a href="#">Alan Taylor</a></p>
-                                <p>Stars: <a href="#">Chris Hemsworth,  </a> <a href="#">  Natalie Portman,</a><a href="#">Tom Hiddleston</a></p>
-                            </div>
-                        </div>
+
                         <div className="topbar-filter">
                             <label>Movies per page:</label>
                             <select>
